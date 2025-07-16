75637dad566f937c66e8976ad4e5416d3c1124d7
Yasathilaka M Kiridana <kavindakiridena@gmail.com>
2025-07-16 16:54:52 +0530

    Add vanillaModel class and Chart.js integration
    Introduced a new vanillaModel class to the project and updated Index.aspx to include Chart.js via CDN and a canvas element for chart rendering. Enhanced Index.aspx.cs to connect to an Excel data source using OleDb, read data from the 'vanilla_farm_data' sheet, and display results in the TextBox. Updated project and designer files to register the new class and form controls.

--- chartJS/chartJS/chartJS/Index.aspx
+++ chartJS/chartJS/chartJS/Index.aspx
@@ -5,11 +5,19 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <!-- Include Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body style="height: 270px">
+        <form id="form1" runat="server">
         <div>
             <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
             <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+            <h3>Chart.js Canvas</h3>
+            <div style="width: 400px; height: 300px;">
+                <canvas id="myChart"></canvas> <!-- Add a <canvas> Element -->
+            </div>
         </div>
+        </form>
 </body>
 </html>
--- chartJS/chartJS/chartJS/Index.aspx.cs
+++ chartJS/chartJS/chartJS/Index.aspx.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,7 +20,43 @@ namespace chartJS
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            using (OleDbConnection conn = new OleDbConnection(ConfigurationManager.ConnectionStrings["ExcelConnection"].ConnectionString))
+            {
+                conn.Open();
+                // Check connection state
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    TextBox1.Text = "connection was successful";
 
+                    string query = "SELECT * FROM [vanilla_farm_data]";//csv file name without extension
+                    using (OleDbCommand cmd = new OleDbCommand(query, conn))
+                    {
+                        using (OleDbDataReader reader = cmd.ExecuteReader())//fix the bug here
+                        {
+                            while (reader.Read())
+                            {
+                                // FIX: Skip the first row if it contains headers
+                                if (reader.HasRows) // Ensure there's at least one row to read
+                                {
+                                    reader.Read(); // This advances the reader past the first row (the header row)
+                                }
+
+                                while (reader.Read())
+                                {
+                                    // Now, the reader will start from the second row, which contains your actual data.
+                                    // You can access your data here, for example:
+                                    // string column1Value = reader["ColumnName"].ToString();
+                                    // int column2Value = Convert.ToInt32(reader["AnotherColumn"].ToString());
+
+                                    // For demonstration, let's just append some data to the TextBox
+                                    TextBox1.Text += Environment.NewLine + "Data Row: " + reader[0].ToString(); // Reads the first column of the current row
+                                }
+                            }
+                        }
+                    }
+                }              
+                conn.Close();
+            }
         }
     }
 }
