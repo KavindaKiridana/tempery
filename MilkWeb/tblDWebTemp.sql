1725ed476a094d20bb259f7ecbfd1e4b6c3abd60
Yasathilaka M Kiridana <kavindakiridena@gmail.com>
2025-08-30 11:45:36 +0530

    Update tblDWebTemp.sql

--- MilkWeb/tblDWebTemp.sql
+++ MilkWeb/tblDWebTemp.sql
@@ -1,15 +1,15 @@
-CREATE TABLE tblDWebTemp (
-    ID INT IDENTITY(1,1) NOT NULL,
-    UserId INT NOT NULL,
-    CCode INT NOT NULL,
-    FarmerCode VARCHAR(20) NOT NULL,
-    OldCode VARCHAR(20) NOT NULL,
-    Parent VARCHAR(20) NOT NULL,
-    DateOf VARCHAR(40) NOT NULL,
-    TimeOf VARCHAR(10) NOT NULL,
-    SNF DECIMAL(5,1) NOT NULL,
-    FAT DECIMAL(5,1) NOT NULL,
-    Qty int NOT NULL,
-    SavedTime DATE DEFAULT GETDATE(),
-    PRIMARY KEY (ID)
+use Dairy_Collector;
+CREATE TABLE [dbo].[tblDWebTemp](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[CCode] [int] NOT NULL,
+	[FarmerCode] [varchar](20) NOT NULL,
+	[OldCode] [varchar](20) NOT NULL,
+	[Parent] [varchar](20) NOT NULL,
+	[DateOf] [varchar](40) NOT NULL,
+	[TimeOf] [varchar](10) NOT NULL,
+	[SNF] [decimal](5, 1) NOT NULL,
+	[FAT] [decimal](5, 1) NOT NULL,
+	[Qty] [decimal](10, 3) NOT NULL,
+	[SavedTime] [date] default CAST(GETDATE() AS DATE)
 );
