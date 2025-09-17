4c7f6b12aef068bdc8941adcc1f3e2605709b6fd
Yasathilaka M Kiridana <kavindakiridena@gmail.com>
2025-09-17 13:27:27 +0530

    Update itasset_db_script.sql

--- IT Asset Purchases/itasset_db_script.sql
+++ IT Asset Purchases/itasset_db_script.sql
@@ -9,7 +9,8 @@ GO
 -- Create Reason table
 CREATE TABLE Reason (
     ReasonId INT IDENTITY(1,1) PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL
+    Name VARCHAR(255) NOT NULL,
+    IsActive BIT NOT NULL DEFAULT(1)
 );
 
 -- Create Company table
@@ -46,26 +47,13 @@ CREATE TABLE [Users] (
     IsAuthorizer VARCHAR(50) CHECK (IsAuthorizer IN ('Unauthorizer', 'IT Manager', 'CEO', 'MD','Editor'))
 );
 
--- Create Template table
-CREATE TABLE Template (
-    TemplateId INT IDENTITY(1,1) PRIMARY KEY,
-    Description VARCHAR(MAX) NULL,
-    IsMDSign BIT NOT NULL,
-    ITManagerId INT NOT NULL,
-    CEOId INT NOT NULL,
-    MDId INT NULL,
-    FOREIGN KEY (ITManagerId) REFERENCES [User](UserId),
-    FOREIGN KEY (CEOId) REFERENCES [User](UserId),
-    FOREIGN KEY (MDId) REFERENCES [User](UserId)
-);
-
 -- Create Document table
 CREATE TABLE Document (
     DocumentId INT IDENTITY(1,1) PRIMARY KEY,
     ReasonId INT NOT NULL,
     CompanyId INT NOT NULL,
     DepartmentId INT NOT NULL,
-    UserId INT NOT NULL, -- edited user
+    UsersId INT NOT NULL, -- edited user
     DepartmentHead INT NOT NULL, -- department Head user
     TemplateId int NOT NULL,
     CategoryId INT NOT NULL;
@@ -87,13 +75,13 @@ CREATE TABLE Document (
     CostBeakdown BIT,
     ConfirmedBy int NOT NULL,
     Location VARCHAR(255) NOT NULL;
-    FOREIGN KEY (ConfirmedBy) REFERENCES [User](UserId),
+    FOREIGN KEY (ConfirmedBy) REFERENCES [User](UsersId),
     FOREIGN KEY (ReasonId) REFERENCES Reason(ReasonId),
     FOREIGN KEY (CompanyId) REFERENCES Company(CompanyId),
     FOREIGN KEY (DepartmentId) REFERENCES Department(DepartmentId),
-    FOREIGN KEY (UserId) REFERENCES [User](UserId),
+    FOREIGN KEY (UsersId) REFERENCES [Users](UsersId),
     FOREIGN key(TemplateId) REFERENCES [FlexibleTemplate](FlexibleTemplateId),
-    FOREIGN KEY (DepartmentHead) REFERENCES [User](UserId),
+    FOREIGN KEY (DepartmentHead) REFERENCES [Users](UsersId),
     FOREIGN KEY (CategoryId) REFERENCES Category(CategoryId)
 );
 
@@ -105,36 +93,28 @@ CREATE TABLE RequestedItemPayments (
     Description VARCHAR(500),
     DocumentID INT NOT NULL,
     SupplierId INT NOT NULL,
+    Currency VARCHAR(50),
     FOREIGN KEY (DocumentID) REFERENCES Document(DocumentId),
     FOREIGN KEY (SupplierId) REFERENCES Supplier(SupplierId)
-);
-
--- Create SignedBy table
-CREATE TABLE SignedBy (
-    DocumentID INT NOT NULL,
-    UserId INT NOT NULL,
-    DateTime DATETIME DEFAULT GETDATE(),
-    Position VARCHAR(255),
-    PRIMARY KEY (DocumentID, UserId),
-    FOREIGN KEY (DocumentID) REFERENCES Document(DocumentId),
-    FOREIGN KEY (UserId) REFERENCES [User](UserId)
 );
 
 
 CREATE TABLE FlexibleTemplate (
     FlexibleTemplateId int IDENTITY(1,1) PRIMARY KEY,
+    CompanyId INT NOT NULL,
     IsActive bit not null DEFAULT(1),
-)
+    FOREIGN KEY (CompanyId) REFERENCES Company(CompanyId)
+);
 
--- Related persons table
 CREATE TABLE PersonPosition (
-    PersonPositionsId int IDENTITY(1,1) PRIMARY KEY,
-    FlexibleTemplateId int FOREIGN KEY REFERENCES FlexibleTemplate(FlexibleTemplateId),
-    Position nvarchar(100),
-    PersonName nvarchar(100),
-)
-
-
+    PersonPositionId INT IDENTITY(1,1) PRIMARY KEY,
+    FlexibleTemplateId int not null,
+    PersonId int not null,
+    Position VARCHAR(50) not null,
+FOREIGN KEY (FlexibleTemplateId) REFERENCES FlexibleTemplate(FlexibleTemplateId),
+FOREIGN KEY (PersonId) REFERENCES Users(UsersId),
+CONSTRAINT UQ_PersonPosition UNIQUE (FlexibleTemplateId, PersonId) -- Composite unique constraint
+);
 
 CREATE TABLE Category (
     CategoryId INT PRIMARY KEY IDENTITY(1,1),
@@ -143,16 +123,7 @@ CREATE TABLE Category (
     IsActive BIT NOT NULL DEFAULT 1
 );
 
-INSERT INTO Category (CategoryName, Discription) VALUES
-('Laptop/Desktop', 'Laptop & Desktop'),
-('Printers/Photocopy', 'Photocopy, All kind of Printers'),
-('UPS', 'UPS'),
-('License', 'All kind of License'),
-('AMC', 'AMC Items'),
-('Rent', 'Rented Laptops or any other'),
-('Monitor', 'Monitors'),
-('Repair', 'Any Item which is sent to repair'),
-('Others', 'Any items which is not listed above');
+
 
 
 
