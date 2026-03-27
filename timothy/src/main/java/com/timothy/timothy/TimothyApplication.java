adec51551e754f040891f28197069007d55fef7e
Yasathilaka M Kiridana <kavindakiridena@gmail.com>
2026-03-27 06:19:58 +0530

    add timothy backup

---
+++ timothy/src/main/java/com/timothy/timothy/TimothyApplication.java
@@ -0,0 +1,13 @@
+package com.timothy.timothy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimothyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimothyApplication.class, args);
+	}
+
+}
---
+++ timothy/src/test/java/com/timothy/timothy/TimothyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timothy.timothy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimothyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
